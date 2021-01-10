5b19926fcf72278a3c95c7469aa975ac430ff983
santto97 <56086446+santto97@users.noreply.github.com>
2021-01-10 03:40:32 -0500

    assignment 2 _ week 3

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,35 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+#create a matriz especial 
+#crear una funcion especial que cree una matriz y defina una variable para
+#guardar en cache su valor invertido 
+makeCacheMatrix <- function(matriz = matrix()){
+  #inicializacion de variables
+  inver <- NULL
+  set <- function(y){
+    matriz <<-y
+    inver <<- NULL
+    
+  }
+  get <- function() matriz
+  setsolve <- function(solve) inver <<- solve
+  getsolve <- function() matriz
+  list(set = set ,get = get, 
+       getsolve = getsolve,
+       setsolve = setsolve)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#verificacion de cache en una funcion
+cacheSolve <- function(matriz, ...){
+  inver <- matriz$getsolve()
+  if(!is.null(inver)){
+    message("getting inversed matrix")
+    return( inver)
+  }
+  data <- matriz$get()
+  inver <- solve(data, ...)
+  matriz$setsolve(inver)
+  inver
 }
+
